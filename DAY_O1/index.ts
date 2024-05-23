1b243be92d8558a6eb899672ff8bab726a5df3e4
S-M-Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-05-23 11:34:47 +0500

    Day 01 of 100

---
+++ DAY_O1/index.ts
@@ -0,0 +1,23 @@
+/*Q1 - COMPLETE THE INSTALLATION OF TYPESCRIPT
+INSTALLLING node.js*-- done
+---------process use----------------
+visit https://nodejs.org/en
+download Node.js ltsc
+--------------------------------------
+INSTALLLING typescript*-- 
+ ----commands use-------------------
+ npm install -g typescript
+ tsc -v
+ -----------------------------------
+INSTALLLING VsCode*-- done
+-------processes use-----------
+visit https://code.visualstudio.com/
+--------------------------------*/
+//Q2- Personal Message
+let personName:string = `Alberto Carlos`;
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
+// Q3 - Name Cases
+let personName2:string = `Alberto carlos`;
+console.log(personName2.toLowerCase());//lowercase
+console.log(personName2.toUpperCase());//uppercase
+console.log(personName2[0].toUpperCase() + personName2.slice(1).toLowerCase());//titlecase
