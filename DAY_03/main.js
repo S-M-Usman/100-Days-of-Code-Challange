9b09f93528c2dcd1ec9180a0bc4a26bd3a27b1f2
S-M-Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-07-13 17:51:35 +0500

    Day 03 of 100

---
+++ DAY_03/main.js
@@ -0,0 +1,14 @@
+//Q7
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(48 / 6);
+//Q8
+console.log(2 + 6);
+console.log(16 - 8);
+console.log(64 / 8);
+console.log(Math.pow(2, 3));
+//Q9
+var favouriteNumber = 0;
+var message = "My favourite number is: \"".concat(favouriteNumber, "\"");
+console.log(favouriteNumber);
---
+++ DAY_03/main.ts
@@ -0,0 +1,15 @@
+//Q7
+console.log(4+4);
+console.log(10-2);
+console.log(4*2);
+console.log(48/6);
+//Q8
+console.log(2+6);
+console.log(16-8);
+console.log(64/8);
+console.log(2**3);
+//Q9
+let favouriteNumber = 0
+let message = `My favourite number is: "${favouriteNumber}"`
+console.log(favouriteNumber);
+
